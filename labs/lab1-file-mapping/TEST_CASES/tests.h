3a43a63c55a8b424aefcd6519146eb4eb3b81ccb
Ivan Grishin <70744430+Warlosn@users.noreply.github.com>
2022-05-15 16:25:28 +0300

    labs 1-3

---
+++ labs/lab1-file-mapping/TEST_CASES/tests.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma comment(lib, "D:/GitHub/6_Sem_OSISP/labs/lab1-file-mapping/x64/Debug/OS10_HTAPI.lib")
+
+#include "../OS10_HTAPI/pch.h"
+#include "../OS10_HTAPI/HT.h"
+
+namespace tests
+{
+	BOOL test1(ht::HtHandle* htHandle);
+	BOOL test2(ht::HtHandle* htHandle);
+	BOOL test3(ht::HtHandle* htHandle);
+	BOOL test4(ht::HtHandle* htHandle);
+}
